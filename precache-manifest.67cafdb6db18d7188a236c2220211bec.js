d5d5bafdf5f0c52256167fdeb992b034232b91bc
Chernysheva Netology <chernysheva@mail.ru>
2021-05-25 21:07:36 +0200

    Updates

---
+++ precache-manifest.67cafdb6db18d7188a236c2220211bec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f796a9086399c14546fb30fccb1c9003",
+    "url": "/chat-history-ts/index.html"
+  },
+  {
+    "revision": "b3a159549e1df3050048",
+    "url": "/chat-history-ts/static/css/main.93656b43.chunk.css"
+  },
+  {
+    "revision": "b712e40da11c589187ab",
+    "url": "/chat-history-ts/static/js/2.c73d96aa.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/chat-history-ts/static/js/2.c73d96aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a159549e1df3050048",
+    "url": "/chat-history-ts/static/js/main.0e97c061.chunk.js"
+  },
+  {
+    "revision": "e374f48d019f3631fba5",
+    "url": "/chat-history-ts/static/js/runtime-main.49dd5ef0.js"
+  }
+]);
